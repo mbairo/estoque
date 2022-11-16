5955bd601df9f76b3c918d3e817eec2f0e1828a0
mbairo <bairo.marcos@gmail.com>
2022-11-16 16:43:08 -0300

    add estoque - RequestMapping

---
+++ estoque/src/main/java/br/com/estoque/estoque/EstoqueApplication.java
@@ -0,0 +1,36 @@
+package br.com.estoque.estoque;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import builder.ConstrutorDePedido;
+import model.Pedido;
+
+@RestController
+@SpringBootApplication
+public class EstoqueApplication {
+
+	// Funcionou, rapaziada!!! hahahah *apagar isso
+	@RequestMapping("/aeee")
+	public String aeee() {
+		return "Aeeeeee!!!!";
+	}
+
+	@RequestMapping("/getPedido")
+	public Pedido mostraPedido() {
+		Pedido pedido = new ConstrutorDePedido ()
+		.setNumeroPedido("1123542134")
+		.setMatriz("CTBA - Centro")
+		.setFilial("SC - Florianópolis")
+		.setProduto("Celular", "Xiaomi - Poco x3", 200)
+		.setProduto("Televisão", "LG - 55 polegadas", 100)
+		.builder();
+	return pedido;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueApplication.class, args);
+	}
+}
